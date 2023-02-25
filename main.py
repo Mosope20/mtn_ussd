bfd27c769abe4c5afc80e7b4d337596573dcd437
Mosope20 <87585884+Mosope20@users.noreply.github.com>
2023-02-25 23:31:23 +0100

    updating the dictionaries

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ first_dict = {
         "8." : "Hot Deals"
     }
 #------------------------------------------------------------------------------------------------------------------------------------------------
-                                                    # option (1) from first_dict
+
+# option (1) from first_dict
 BuyData = {
         "1." : "Daily",
         "2." : "Weekly",
@@ -39,8 +40,93 @@ Daily = {
         "9." : "N300 for 750MB(3-day plan)",
         "10." : "N25 for WhatsApp Daily",
         "11." : "N50 Jolly Combo Offer",
-        "12.": "N100 Jolly Combo Offer"
+        "12.": "N100 Jolly Combo Offer",
+        "0." : "Back"
     }
+
+Weekly = {
+        "1." : "N200 for 1GB(IG/TIKTOK/Youtube ONLY)",
+        "2." : "N300 for 350MB",
+        "3." : "N500 for 1.5GB",
+        "4." : "N500 for 750MB(2-Week plan)",
+        "5." : "N1500 for 6GB",
+        "6." : "N1000 for 2GB",
+        "7." : "N500 for 1GB",
+        "8." : "N50 for WhatsApp Weekly",
+        "9." : "N500 for 750MB+1hr YouTube",
+        "0." : "Back"
+    }
+
+Monthly = {
+        "1." : "N1,000 for 1.5GB",
+        "2." : "N1200 for 2GB",
+        "3." : "N1,500 for 3GB",
+        "4." : "N2,000 for 4.5GB",
+        "5." : "N2,500 for 6GB",
+        "6." : "N3,500 for 12GB",
+        "7." : "N5,000 for 20GB",
+        "8." : "N6,000 for 25GB",
+        "9." : "N10,000 for 40GB",
+        "10." : "N15,000 for 75GB",
+        "11." : "N20,000 for 120GB",
+        "12." : "N30,000 for 200GB",
+        "13." : "N3,000 for 10GB",
+        "0." : "Back"
+    }
+
+Multi_Month = {
+        "1." : "N8,000 for 30GB\\60Days",
+        "2." : "N20,000 for 100GB\\60Days",
+        "3." : "N30,000 for 160GB\\60Days",
+        "4." : "N50,000 for 400GB\\90Days",
+        "5." : "N75,000 for 600GB\\90Days",
+        "0." : "Back"
+        }
+
+Always_On = {
+        "1." : "N60 for 200MB/24 Hrs",
+        "2." : "N120 for 450MB/ 7 Days",
+        "3." : "N3000 for 15GB/ 30 Days",
+        "4." : "N6000 for 45GB/ 30Days",
+        "0." : "Back"
+        }
+
+Broadband = {
+        "1." : "Link Device",
+        "2." : "Recharge Device",
+        "3." : "Buy Data Bundle on Device",
+        "4." : "Check Device Balance",
+        "0." : "View & Unlink Device"
+        }
+
+Family = {
+        "1." : "Buy Family Pack",
+        "2." : "Add New Beneficiary/Sponsor",
+        "3." : "Manage Beneficiary/Sponsor",
+        "4." : "Balance Enquiry",
+        "5." : "Deactivate Service",
+        "6." : "Help",
+        "0." : "Back"
+        }
+
+Hot_Deals = {
+        "1." : "N500- 750MB+N500 Talktime. Val/14days",
+        "2." : "N1,000- 1.5GB+N1000 Talktime. Valid 30days",
+        "3." : "N2,000- 4.5GB+N2000 Talktime. Val/30days",
+        "0." : "Back"
+        }
+
+Mobile_5G = {
+        "1." : "N5,000 for 20GB",
+        "2." : "N6,000 for 25GB",
+        "3." : "N10,000 for 40GB",
+        "4." : "N15,000 for 75GB",
+        "5." : "N20,000 for 120GB",
+        "6." : "N30,000 for 200GB",
+        "7." : "N20,000 for 100GB",
+        "0." : "Back"
+
+        }
 
 
 
