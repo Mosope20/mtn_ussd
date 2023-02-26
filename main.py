0d4bcc2bab1e8639cd712e4a7e2e13797d620719
Mosope20 <87585884+Mosope20@users.noreply.github.com>
2023-02-26 21:17:47 +0100

    adding comments

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ Daily = {
         "12.": "N100 Jolly Combo Offer",
         "0." : "Back"
     }
-
+# option (2) from BuyData
 Weekly = {
         "1." : "N200 for 1GB(IG/TIKTOK/Youtube ONLY)",
         "2." : "N300 for 350MB",
@@ -56,7 +56,7 @@ Weekly = {
         "9." : "N500 for 750MB+1hr YouTube",
         "0." : "Back"
     }
-
+# option (3) from BuyData
 Monthly = {
         "1." : "N1,000 for 1.5GB",
         "2." : "N1200 for 2GB",
@@ -73,7 +73,7 @@ Monthly = {
         "13." : "N3,000 for 10GB",
         "0." : "Back"
     }
-
+# option (4) from BuyData
 Multi_Month = {
         "1." : "N8,000 for 30GB\\60Days",
         "2." : "N20,000 for 100GB\\60Days",
@@ -82,7 +82,7 @@ Multi_Month = {
         "5." : "N75,000 for 600GB\\90Days",
         "0." : "Back"
         }
-
+# option (5) from BuyData
 Always_On = {
         "1." : "N60 for 200MB/24 Hrs",
         "2." : "N120 for 450MB/ 7 Days",
@@ -90,7 +90,7 @@ Always_On = {
         "4." : "N6000 for 45GB/ 30Days",
         "0." : "Back"
         }
-
+# option (6) from BuyData
 Broadband = {
         "1." : "Link Device",
         "2." : "Recharge Device",
@@ -98,7 +98,7 @@ Broadband = {
         "4." : "Check Device Balance",
         "0." : "View & Unlink Device"
         }
-
+# option (7) from BuyData
 Family = {
         "1." : "Buy Family Pack",
         "2." : "Add New Beneficiary/Sponsor",
@@ -108,14 +108,14 @@ Family = {
         "6." : "Help",
         "0." : "Back"
         }
-
+# option (8) from BuyData
 Hot_Deals = {
         "1." : "N500- 750MB+N500 Talktime. Val/14days",
         "2." : "N1,000- 1.5GB+N1000 Talktime. Valid 30days",
         "3." : "N2,000- 4.5GB+N2000 Talktime. Val/30days",
         "0." : "Back"
         }
-
+# option (9) from BuyData
 Mobile_5G = {
         "1." : "N5,000 for 20GB",
         "2." : "N6,000 for 25GB",
@@ -126,12 +126,15 @@ Mobile_5G = {
         "7." : "N20,000 for 100GB",
         "0." : "Back"
         }
+
+# option (10) from BuyData
 YouTube = {
         "1." : "Weekly Plan",
         "2." : "Monthly Plan",
         "0." : "Back"
         }
 
+# option (11) from BuyData
 Manage_Data = {
         "1." : "View Active Data Bundle",
         "2." : "Opt in for Auto renewal",
